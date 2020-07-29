b4c0d229f6ba5f4bb42ba07268bfbeca10d785d8
dongxiang <672377968@qq.com>
2020-07-29 01:29:34 -0700

    驱动模板代码优化

--- driver_case/2.0/driver_case.c
+++ driver_case/2.0/driver_case.c
@@ -12,37 +12,53 @@
 ********************************************************************************
 */
 
-#include <linux/cdev.h>
-#include <linux/module.h>
 #include <linux/of.h>
-#include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 #include <linux/slab.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
 
-#define DEV_MAJOR 201
-#define DEV_MINOR 0
-#define DEV_NUM   1
-#define ERROR    -1
-#define OK        0
+//#define DEV_MAJOR  201
+#define DEV_MINOR    0
+#define ERROR       -1
+#define OK           0
 
-#define DRIVER_CASE_CLASS_NAME "driver_case_class"
+#define DRIVER_CASE_NUM          1
+#define DRIVER_CASE_CLASS_NAME   "driver_case_class"
 #define DRIVER_CASE_DEVICE_NAME  "driver_case"
-#define DRIVER_CASE_NAME "driver_case"
-#define PLATFORM_NAME "platform_driver_case"
-#define COMPATABLE_NAME "jimi,driver_case"
-#define DRIVER_CASE_NUM 1
+#define DRIVER_CASE_NAME         "driver_case"
+#define PLATFORM_NAME            "platform_driver_case"
+#define COMPATABLE_NAME          "jimi,driver_case"
+
 
 //#define DECLARE_DRIVER_CASE_SUPPORT 
+#define DEBUG_LOG_SUPPORT
+
+#define PRINT_ERR(format,x...)    \
+do{ printk(KERN_ERR "ERROR: func: %s line: %04d info: " format,          \
+                                 __func__, __LINE__, ## x); }while(0)
+#define PRINT_INFO(format,x...)   \
+do{ printk(KERN_INFO "[driver_case]" format, ## x); }while(0)
+
+#ifdef DEBUG_LOG_SUPPORT
+#define PRINT_DEBUG(format,x...)  \
+do{ printk(KERN_INFO "[driver_case] func: %s line: %d info: " format,    \
+                                 __func__, __LINE__, ## x); }while(0)
+#else
+#define PRINT_DEBUG(format,x...)  
+#endif
 
 struct driver_case_platform_data {
-    int    major;
     int    gpio_num;
+};
+
+struct driver_case_dev {
+    int    major;
     dev_t  devid;
     struct cdev cdev;
     struct class *class;
     struct device *device;
-};
-
-struct  driver_case_dev {
     struct driver_case_platform_data *platform_data;
 };
 
@@ -53,7 +69,7 @@ static int driver_case_open(struct inode *inode, struct file *filp)
     return 0;
 }
 
-static ssize_t driver_case_write(struct file *file, const char __user *buf, 
+static ssize_t driver_case_write(struct file *file, const char __user *buf,
                              size_t size, loff_t *offset)
 {
     return 0;
@@ -68,39 +84,37 @@ static struct file_operations driver_case_fops = {
 
 static int register_driver(void)
 {
-    printk("%s:%d: Entry %s \r\n", __FILE__, __LINE__, __func__);
+    PRINT_INFO("Entry %s \n", __func__);
     /* 1. 设置设备号 
      * 主设备号已知， 静态注册；未知， 动态注册。
      */
-#ifdef DEV_MAJOR    
-        printk("%s:%d -----------DEV_MAJOR------------", __func__, __LINE__);
-        driver_case.platform_data->devid = MKDEV(DEV_MAJOR, 0);
-        register_chrdev_region(driver_case.platform_data->devid, DRIVER_CASE_NUM, DRIVER_CASE_NAME);
-        printk("%s:%d DEV_MAJOR", __func__, __LINE__);
+#ifdef DEV_MAJOR
+        driver_case.devid = MKDEV(DEV_MAJOR, 0);
+        register_chrdev_region(driver_case.devid, DRIVER_CASE_NUM, DRIVER_CASE_NAME);
 #else    
-        alloc_chrdev_region(&driver_case.platform_data->devid, 0, DRIVER_CASE_NUM, DRIVER_CASE_NAME);
-        driver_case.platform_data->major = MAJOR(driver_case.platform_data->devid);    
+        alloc_chrdev_region(&driver_case.devid, 0, DRIVER_CASE_NUM, DRIVER_CASE_NAME);
+        driver_case.major = MAJOR(driver_case.devid);    
 #endif
 
     /* 2. 注册驱动结构体 */
-    driver_case.platform_data->cdev.owner = THIS_MODULE;
-    cdev_init(&driver_case.platform_data->cdev, &driver_case_fops);
-    cdev_add(&driver_case.platform_data->cdev, driver_case.platform_data->devid, DRIVER_CASE_NUM);
-    printk("Failed:%s:%d driver_case_fops succesful! \r\n", 
-                __func__, __LINE__);
+    driver_case.cdev.owner = THIS_MODULE;
+    cdev_init(&driver_case.cdev, &driver_case_fops);
+    cdev_add(&driver_case.cdev, driver_case.devid, DRIVER_CASE_NUM);
+    PRINT_DEBUG("driver_case_fops succesful! \n");
+    
     /* 3. 创建类 */
-    driver_case.platform_data->class = class_create(THIS_MODULE, DRIVER_CASE_CLASS_NAME);    
-    if(IS_ERR(driver_case.platform_data->class)) {
-        printk("Failed:%s:%d: %s under class created failed! \r\n", 
-                __func__, __LINE__, DRIVER_CASE_DEVICE_NAME);
+    driver_case.class = class_create(THIS_MODULE, DRIVER_CASE_CLASS_NAME);    
+    if(IS_ERR(driver_case.class)) {
+        PRINT_ERR("%s under class created failed! \n", DRIVER_CASE_DEVICE_NAME);
+        
         return ERROR;
     }
+    
      /* 4.创建设备 */
-    driver_case.platform_data->device = device_create(driver_case.platform_data->class, NULL, 
-                                    driver_case.platform_data->devid, NULL, DRIVER_CASE_DEVICE_NAME);
-    if(NULL == driver_case.platform_data->device) {
-        printk("Failed:%s:%d: %s device created failed! \r\n", 
-                __func__, __LINE__,  DRIVER_CASE_DEVICE_NAME);    
+    driver_case.device = device_create(driver_case.class, NULL, 
+                                    driver_case.devid, NULL, DRIVER_CASE_DEVICE_NAME);
+    if(NULL == driver_case.device) {   
+        PRINT_ERR("%s device created failed! \n", DRIVER_CASE_DEVICE_NAME); 
         return ERROR;    
     }
 
@@ -115,7 +129,8 @@ static struct driver_case_platform_data  *driver_case_parse_dt(struct device *pd
 
     pdata = kzalloc(sizeof(*pdata), GFP_KERNEL);
     if (!pdata) {
-        //dev_err(dev, "could not allocate memory for platform data\n");
+        PRINT_ERR("could not allocate memory for platform data\n");
+
         return NULL;
     }
 
@@ -127,20 +142,20 @@ static int driver_case_probe(struct platform_device *pdev)
 {
     int ret = -1 ;
     struct driver_case_platform_data *pdata = pdev->dev.platform_data;
-    struct driver_case_dev *pdriver_case = &driver_case;
+    struct driver_case_dev *pdriver_case = &driver_case;    
+    PRINT_INFO("Entry %s \n", __func__);
     
-    printk("%s:%d: Entry %s \r\n", __FILE__, __LINE__, __func__);
-
     if (!pdata) {
-        printk("could not allocate memory for platform data\n");
+        PRINT_ERR("could not allocate memory for platform data\n");
         pdata = driver_case_parse_dt(&pdev->dev);
         if(pdata) {
             pdev->dev.platform_data = pdata;
         }
     }    
     if (!pdata) {
-        printk(KERN_WARNING "get platform_data NULL\n");
+        PRINT_ERR("get platform_data NULL\n");
         ret = -EINVAL;
+        
         goto fail_to_get_platform_data;
     }    
     
@@ -148,10 +163,10 @@ static int driver_case_probe(struct platform_device *pdev)
     
     ret = register_driver();
     if(ERROR == ret ) {
-        printk("Failed:%s:%d: driver register error! \r\n", __func__, __LINE__);
+        PRINT_ERR("driver register error! \r\n");
         goto fail_to_get_platform_data;
     }  else {
-        printk("%s:%d: driver register successfully \r\n", __func__, __LINE__);
+        PRINT_INFO("driver register successfully \n");
     }
 
     return 0;
@@ -162,10 +177,10 @@ fail_to_get_platform_data:
 
 int driver_case_remove(struct platform_device *pdev)
 {
-    cdev_del(&driver_case.platform_data->cdev);
-    unregister_chrdev_region(driver_case.platform_data->devid, DRIVER_CASE_NUM);    
-    device_destroy(driver_case.platform_data->class, driver_case.platform_data->devid);
-    class_destroy(driver_case.platform_data->class);
+    cdev_del(&driver_case.cdev);
+    unregister_chrdev_region(driver_case.devid, DRIVER_CASE_NUM);    
+    device_destroy(driver_case.class, driver_case.devid);
+    class_destroy(driver_case.class);
 
     return 0;
 }
@@ -191,8 +206,7 @@ static struct platform_driver driver_case_device_driver = {
 
 static int __init driver_case_init(void)
 {
-    printk("%s:%d: Entry %s \r\n", __FILE__, __LINE__, __func__);
-
+    PRINT_INFO("Entry %s \n", __func__);
     platform_driver_register(&driver_case_device_driver);
     
     return 0;
@@ -200,8 +214,7 @@ static int __init driver_case_init(void)
 
 static void __exit driver_case_exit(void)
 {
-    printk("%s:%d: Entry %s \r\n", __FILE__, __LINE__, __func__);
-
+    PRINT_INFO("Entry %s \n", __func__);
     platform_driver_unregister(&driver_case_device_driver);
 }
 
